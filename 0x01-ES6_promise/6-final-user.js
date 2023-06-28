28624c822ca5be3bfa97f727cb3a5d6c5b7444cf
Wangari <109669003+karreycodes@users.noreply.github.com>
2023-06-28 21:12:40 +0300

    rejecting an error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,26 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [];
-
-  promises.push(signUpUser(firstName, lastName));
-  promises.push(uploadPhoto(fileName));
-
-  const results = [];
-
-  return Promise.allSettled(promises)
-    .then((settledPromises) => {
-      for (let i = 0; i < settledPromises.length; i += 1) {
-        const result = settledPromises[i];
-        const { status } = result;
-        const value = status === 'fulfilled' ? result.value : result.reason;
-
-        results.push({
-          status,
-          value,
-        });
-      }
-
-      return results;
-    });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => data.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
