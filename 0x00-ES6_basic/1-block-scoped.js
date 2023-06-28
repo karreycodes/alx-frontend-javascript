52bafb3d098f918e1b8d9345aef3e92a788c3641
karreycodes <kamondewangari@gmail.com>
2023-06-28 06:39:52 -0700

    let const

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
